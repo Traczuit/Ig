20872801cb576f4803f3816e0bc626dc85410724
Mauricio <traczm93@hotmail.com>
2021-02-10 21:08:55 -0300

    firebase imported + config

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import * as firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyBnt1zoXWGz--9leVo0etmwU1l8t2n2hio",
+  authDomain: "ig-demo-b8803.firebaseapp.com",
+  projectId: "ig-demo-b8803",
+  storageBucket: "ig-demo-b8803.appspot.com",
+  messagingSenderId: "348158837085",
+  appId: "1:348158837085:web:2fb50eb220a830930fe8b4",
+  measurementId: "G-ZGWFYV86ZL"
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig);
+}
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LandingScreen from "./components/auth/Landing";
+import RegisterScreen from "./components/auth/Register";
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -14,6 +30,10 @@ export default function App() {
           name="Landing"
           component={LandingScreen}
           options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react'
-import {View, Button, TextInput} from 'react-native'
-export class Register extends Component {
-    constructor(props){
-        super(props);
+import React, { Component } from "react";
+import { View, Button, TextInput } from "react-native";
+import firebase from 'firebase';
+export class RegisterScreen extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: '',
-            name:''
-        }
-    }
-    render() {
-        return (
-            <View>
-                <TextInput  
-                    placeholder='name'
-                    onChangeText={(name) => this.setState({ name })}
-                />
-            </View>
-        )
-    }
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+    };
+    this.onSignUp = this.onSignUp.bind(this)
+  }
+
+  onSignUp(){
+    const { email, password, name } = this.state;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder="name"
+          onChangeText={(name) => this.setState({ name })}
+        />
+        <TextInput
+          placeholder="email"
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Button onPress={() => this.onSignUp()} title="Sign Up" />
+      </View>
+    );
+  }
 }
 
-export default Register
+export default RegisterScreen;
